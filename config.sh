f2a9ea1b837139bacbfa9fd572449d27030e45e2
Cuichanghe <1337985150@qq.com>
2021-04-23 16:09:27 +0800

    V0.5

--- config.sh
+++ config.sh
@@ -2,7 +2,8 @@
 #一键增加容器脚本(v0.1) Created By Cuichanghe
 USER_PROT=$1
 CS_PASSWORD=$2
-echo bind-addr: 0.0.0.0:${USER_PORT}>config.yaml;
+cd /root/.config/code-server/
+echo bind-addr: 0.0.0.0:${USER_PROT} >config.yaml;
 if [ -z "$CS_PASSWORD" ]; then
 echo 检测到无code-server登录密码
 echo auth: none >> config.yaml;
